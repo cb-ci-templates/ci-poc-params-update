095ef6f549094f91fc704e6e61f0e3fa5a175f57
cccaternberg <acaternberg@cloudbees.com>
2024-03-22 21:12:40 +0100

    add

--- script-curl-branches.sh
+++ script-curl-branches.sh
@@ -5,7 +5,7 @@ GH_ACCESS_TOKEN=${1:-YOUR_GITHUB_TOKEN}
 REPO_BRANCH=${2:-"https://api.github.com/repos/org-caternberg/dsl-params-update/branches"}
 env | sort
 echo $PATH
-RESULT=$(/usr/bin/curl -L \
+RESULT=$(curl -L \
  -H "Accept: application/vnd.github+json" \
  -H "Authorization: Bearer ${GH_ACCESS_TOKEN}" \
  -H "X-GitHub-Api-Version: 2022-11-28" \
