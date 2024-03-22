162a4e212f6d050dfde2b6b63552ed183a2b0e23
cccaternberg <acaternberg@cloudbees.com>
2024-03-22 21:28:40 +0100

    add

--- Jenkinsfile-DSL-Seed.groovy
+++ Jenkinsfile-DSL-Seed.groovy
@@ -28,7 +28,7 @@ pipeline {
             steps {
                 container("shell") {
                     script {
-                        branches=sh(script: "./script-curl-branches.sh $GH_ACCESS_TOKEN  $REPO_BRANCH  |jq  '.[] | .name' | tr '\\n' ', ' | sed 's/,\$//'", returnStatus: true).trim()
+                        branches=sh(script: "./script-curl-branches.sh $GH_ACCESS_TOKEN  $REPO_BRANCH  |jq  '.[] | .name' | tr '\\n' ', ' | sed 's/,\$//'", returnStatus: true)
                         println "BRANCHES: ${branches}"
                     }
                     //echo sh(script: 'env|sort', returnStdout: true)
