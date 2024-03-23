a900d2d6498d3a2f639105f1037c89a414edcef6
cccaternberg <acaternberg@cloudbees.com>
2024-03-23 10:54:30 +0100

    add

--- updateParams.groovy
+++ updateParams.groovy
@@ -4,8 +4,34 @@ ArrayList elements = myparam.split(',')
 // Create ArrayList and add elements
 def arrayList = new ArrayList(elements)
 println arrayList
-job('example') {
+job('example-dsl-freestylejob') {
     parameters {
         choiceParam('OPTION',arrayList)
+    }
+    steps {
+        shell("echo OPTION SELECTED: ${OPTION}")
+    }
+}
+
+
+pipelineJob('example-dsl-pipelinejob') {
+    parameters {
+        choiceParam('OPTION',arrayList)
+    }
+    definition {
+        //Jenkinsfile should better be in SCM but for demo purpose we make it here inline
+        //see https://jenkinsci.github.io/job-dsl-plugin/#path/pipelineJob-definition-cpsScm-scm-git
+        cps {
+            script('''pipeline {
+                agent none
+                stages {
+                    stage('selectedOption') {
+                        steps {
+                            echo "OPTION SELECTED: ${OPTION}"
+                        }
+                    }
+                }
+            }''')
+        }
     }
 }
