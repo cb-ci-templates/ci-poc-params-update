51303b610c260d018a34ec600bc46c1be1ddf000
cccaternberg <acaternberg@cloudbees.com>
2024-03-23 16:13:13 +0100

    add

---
+++ Jenkinsfile-updateParameters-CasC.groovy
@@ -0,0 +1,45 @@
+library identifier: 'ci-shared-library@main', retriever: modernSCM(
+        [$class: 'GitSCMSource',
+         remote: 'https://github.com/cb-ci-templates/ci-shared-library.git'])
+
+pipeline {
+    agent {
+        kubernetes {
+            yaml '''
+                apiVersion: v1
+                kind: Pod
+                spec:
+                  containers:
+                  - name: shell
+                    image: softonic/curl-jq:3.18.2-1
+                    command:
+                      - cat
+                    tty: true
+                    workingDir: "/home/jenkins/agent"
+                    securityContext:
+                      runAsUser: 1000
+                '''
+            defaultContainer 'shell'
+        }
+    }
+    environment {
+        GH_ACCESS_TOKEN = credentials("github-token")
+        JENKINS_TOKEN = credentials("jenkins-token")
+        REPO_BRANCH = "https://api.github.com/repos/org-caternberg/dsl-params-update/branches"
+        //GIT_REMOTE_BRANCHES = "one, two, three" // For testing purpose
+    }
+    stages {
+        stage('SeedDSL') {
+            steps {
+                container("shell") {
+                    dir ("resources"){
+                        //Shared Lib collects al remote branches and exposes to env.GIT_REPO_BRANCHES}
+                        getGitBranches("$GH_ACCESS_TOKEN","$REPO_BRANCH")
+                        sh "script:casc-updateJobParams.sh ${JENKINS_TOKEN} ${env.GIT_REPO_BRANCHES}"
+                    }
+                }
+            }
+        }
+    }
+}
+
