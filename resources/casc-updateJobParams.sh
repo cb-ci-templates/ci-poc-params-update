5537dd72a01ba9536cec35a5faf674fccde14325
cccaternberg <acaternberg@cloudbees.com>
2024-03-23 12:25:35 +0100

    add

--- resources/casc-updateJobParams.sh
+++ resources/casc-updateJobParams.sh
@@ -1,23 +1,38 @@
 #! /bin/bash
 
-source ./envvars.sh
 
-echo "USage: $0: testcontroller mytestfolder"
+
+echo "USage: $0: testcontroller myjobname"
 
 export CONTROLLER_NAME=${1:-$CONTROLLER_NAME}
 export CONTROLLER_URL=${BASE_URL}"/"${CONTROLLER_NAME}
-export FOLDER_NAME=${2:-$CONTROLLER_NAME}
-GEN_DIR=gen
-rm -rf $GEN_DIR
-mkdir -p $GEN_DIR
+export JOB_NAME=${2:-$JOB_NAME}
+export JENKINS_TOKEN=${3:-"user:token"}
+export NEW_VALUE_STRING=${4:-'["value1", "value2"]'}
+export RESOURCES_DIR=resources
 
-# We render the CasC template instances for the casc-folder (target folder)
-# All variables from the envvars.sh will be substituted
-envsubst < ${CREATE_MM_FOLDER_TEMPLATE_YAML} > $GEN_DIR/${CONTROLLER_NAME}-folder.yaml
+#items:
+#  - kind: pipeline
+#    name: ${JOB_NAME}
+#    concurrentBuild: true
+#    properties:
+#      - parameters:
+#          parameterDefinitions:
+#            - choice:
+#                name: OPTION
+#                choices:
+#                  - dev
+#                  - main
+#    resumeBlocked: false
 
-echo "------------------  CREATING MANAGED CONTROLLER ------------------"
-curl -v -XPOST \
-   --user $TOKEN \
-   "${CONTROLLER_URL}/casc-items/create-items" \
-    -H "Content-Type:text/yaml" \
-   --data-binary @$GEN_DIR/${CONTROLLER_NAME}-folder.yaml
+
+#yq  '.items[0].properties[0].parameters.parameterDefinitions[0].choice.choices = ["dev","test"]' tmp-casc-pipelinejob.yaml
+yq  '.items[0].properties[0].parameters.parameterDefinitions[0].choice.choices = ["dev","test"]' ./casc-pipelinejob.yaml
+yq  '.items[0].parameters[0].choice.choices = ["dev","test"]' ./casc-freestylejob.yaml
+
+echo "------------------  CREATE/UPDATE JOBS------------------"
+#curl -v -XPOST \
+#   --user $TOKEN \
+#   "${CONTROLLER_URL}/casc-items/create-items" \
+#    -H "Content-Type:text/yaml" \
+#   --data-binary @$RESOURCES_DIR/casc-freestylejob.yaml
