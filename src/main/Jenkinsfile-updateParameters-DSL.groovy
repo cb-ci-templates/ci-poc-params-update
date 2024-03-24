fc677c2f1aca9d666c9a457a877ecd6026befe43
cccaternberg <acaternberg@cloudbees.com>
2024-03-24 19:01:51 +0100

    refactor

--- src/main/Jenkinsfile-updateParameters-DSL.groovy
+++ src/main/Jenkinsfile-updateParameters-DSL.groovy
@@ -31,7 +31,7 @@ pipeline {
         stage('UpdateParams') {
             steps {
                 container("shell") {
-                    jobDsl targets: ['./resources/jobdsl_updateJobParams.groovy'].join('\n'),
+                    jobDsl targets: ['./jobdsl_updateJobParams.groovy'].join('\n'),
                             removedJobAction: 'DELETE',
                             removedViewAction: 'DELETE',
                             lookupStrategy: 'SEED_JOB',
