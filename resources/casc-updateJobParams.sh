9c051c2dcbb419510ad536805560c6a66e76f5bf
cccaternberg <acaternberg@cloudbees.com>
2024-03-24 09:39:19 +0100

    update

--- resources/casc-updateJobParams.sh
+++ resources/casc-updateJobParams.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 
-
 echo "Usage: $0: JENKINS_TOKEN CHOICE_VALUES"
+echo 'Example: : casc-updateJobParams.sh youradminuser:yourjenkinstoken '["value1","value2"]''
 echo "Call: $0 $@"
 export CONTROLLER_URL=${JENKINS_URL}
 export JENKINS_TOKEN=${1:-"user:token"}
@@ -18,9 +18,6 @@ function updateJob(){
      --data-binary @$1
 }
 
-YQ=".items[0].properties[0].parameters.parameterDefinitions[0].choice.choices = $PARAM_CHOICE_VALUES"
-echo $YQ
-#yq 'eval(env(YQ))' ./casc-pipelinejob.yaml > ./updated-casc-pipelinejob.yaml
 yq '.items[0].properties[0].parameters.parameterDefinitions[0].choice.choices = env(PARAM_CHOICE_VALUES)' ./casc-pipelinejob.yaml > ./updated-casc-pipelinejob.yaml
 diff -u ./casc-pipelinejob.yaml ./updated-casc-pipelinejob.yaml
 updateJob updated-casc-pipelinejob.yaml
