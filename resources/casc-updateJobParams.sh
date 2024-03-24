3f0e34f21c786a725d654f0d313dbbea43a33881
cccaternberg <acaternberg@cloudbees.com>
2024-03-24 10:49:27 +0100

    update

--- resources/casc-updateJobParams.sh
+++ resources/casc-updateJobParams.sh
@@ -7,7 +7,7 @@ echo "Call: $0 $@"
 export CONTROLLER_URL=${JENKINS_URL}
 export JENKINS_TOKEN=${1:-"user:token"}
 export PARAM_CHOICE_VALUES=${2:-'["new1", "mew2"]'}
-//We got JOB_NAME from Jenkins, we must delete the last path entry to get the Folder
+#We got JOB_NAME from Jenkins, we must delete the last path entry to get the Folder
 export JOB_FOLDER=${3:-"$(echo "$JOB_NAME" | sed 's|/[^/]*$|/|')"}
 
 function updateJob(){
@@ -24,6 +24,7 @@ yq '.items[0].properties[0].parameters.parameterDefinitions[0].choice.choices = 
 diff -u ./casc-pipelinejob.yaml ./updated-casc-pipelinejob.yaml
 updateJob updated-casc-pipelinejob.yaml
 
+#For Freestyle jobs just the yq path is a bit different
 #yq -i '.items[0].parameters[0].choice.choices = env(PARAM_CHOICE_VALUES)' ./casc-freestylejob.yaml > ./updated-casc-freestylejob.yaml
 #cat ./casc-freestylejob.yaml
 #updateJob ./casc-freestylejob.yaml
