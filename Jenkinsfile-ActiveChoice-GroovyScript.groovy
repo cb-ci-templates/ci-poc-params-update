677dd3ce2b09a02cc9287a27ee60db864ca1cd6a
cccaternberg <acaternberg@cloudbees.com>
2024-03-25 16:14:53 +0100

    update URL

--- Jenkinsfile-ActiveChoice-GroovyScript.groovy
+++ Jenkinsfile-ActiveChoice-GroovyScript.groovy
@@ -15,8 +15,8 @@ def SECRET = com.cloudbees.plugins.credentials.SystemCredentialsProvider.getInst
 def URL = "https://" + SECRET + "@github.com/cb-ci-templates/ci-poc-params-update.git"
 def result = ["/bin/bash", "-c", "git ls-remote -h " + URL + " | sed 's/.*refs\\\\/heads\\\\/\\\\(.*\\\\)/\\\\1/'"].execute().text.tokenize();
 return result                                       ''']
-                )
-        )
+                                )
+                    )
         ]
 ),
             [$class: 'JobLocalConfiguration', changeReasonComment: '']])
@@ -35,15 +35,7 @@ pipeline {
                     tty: true
                     workingDir: "/home/jenkins/agent"
                     securityContext:
-                      runAsUser: 1000
-                  - name: git
-                    image: bravissimolabs/alpine-git
-                    command:
-                    - cat
-                    tty: true
-                    workingDir: "/home/jenkins/agent"
-                    securityContext:
-                      runAsUser: 0               
+                      runAsUser: 1000            
                 '''
             defaultContainer 'shell'
         }
@@ -53,31 +45,6 @@ pipeline {
             steps {
                 container("shell") {
                     echo "Hello ${mychoice}"
-                }
-            }
-        }
-        stage('UpdateParamsInRepo') {
-            options {
-                skipDefaultCheckout()
-            }
-            steps {
-                container("git") {
-                    git branch: 'main', changelog: false, credentialsId: 'github-user-ssh', poll: false, url: 'git@github.com:cb-ci-templates/ci-poc-params-update.git'
-                    withCredentials([sshUserPrivateKey(credentialsId: 'github-user-ssh', keyFileVariable: 'CERT', usernameVariable: 'SSH_USER')]) {
-                        sh """
-                            mkdir -p ~/.ssh && chmod 700 ~/.ssh &&  cp -prf $CERT ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
-                            git config --global user.email \"acaternberg@cloudbees.com\"
-                            git config --global user.name $SSH_USER
-                            eval `ssh-agent -s`  && ssh-add ~/.ssh/id_rsa
-                            ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-                            echo \$(date +\"%Y-%m-%d,%H:%M:%S\")  > resources/choices.txt
-                            sleep 1
-                            echo \$(date +\"%Y-%m-%d,%H:%M:%S\")  >> resources/choices.txt
-                            git add resources/choices.txt
-                            git commit -m \"update value\"
-                            git push origin main
-                         """
-                    }
                 }
             }
         }
