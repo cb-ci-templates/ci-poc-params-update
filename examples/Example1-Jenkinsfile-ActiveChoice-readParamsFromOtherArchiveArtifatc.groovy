7e0bd95bfdbdb12fd95b2e9a772e3e63fb158268
cccaternberg <acaternberg@cloudbees.com>
2024-03-27 14:58:13 +0100

    rename

--- examples/Example1-Jenkinsfile-ActiveChoice-readParamsFromOtherArchiveArtifatc.groovy
+++ examples/Example1-Jenkinsfile-ActiveChoice-readParamsFromOtherArchiveArtifatc.groovy
@@ -15,9 +15,7 @@ properties([parameters(
                                              def CREDENTIAL_ID = "jenkins-token"
                                              def SECRET = com.cloudbees.plugins.credentials.SystemCredentialsProvider.getInstance().getStore().getCredentials(com.cloudbees.plugins.credentials.domains.Domain.global()).find { it.getId().equals(CREDENTIAL_ID) }.getSecret().getPlainText()
                                              def url = "https://"+ SECRET + "@sda.acaternberg.flow-training.beescloud.com/sb/job/ci-templates-demo/job/DEMO-ParameterUsage/job/initData/lastSuccessfulBuild/artifact/newparams.txt/*view*/"
-                                             println url
                                              def result = ["/bin/bash", "-c", "curl -L " + url].execute().text.tokenize();
-                                             println result
                                              return result
                                     ''']
                 )
