646b30072d8f81eb1db214c85321964678dde567
cccaternberg <acaternberg@cloudbees.com>
2025-06-13 13:18:44 +0200

    update

--- Jenkinsfile-ActiveChoice-GroovyScript.groovy
+++ Jenkinsfile-ActiveChoice-GroovyScript.groovy
@@ -7,21 +7,19 @@ properties([parameters(
                         fallbackScript: [classpath: [],
                                          oldScript: '',
                                          sandbox  : false, script: 'return  false'],
-                                script: [classpath: [],
-                                         oldScript: '',
-                                         sandbox  : false, script: '''
+                        script: [classpath: [],
+                                 oldScript: '',
+                                 sandbox  : false, script: '''
                                                     def CREDENTIAL_ID = "gh-token-ci-templates-repo-classic"
                                                     def SECRET = com.cloudbees.plugins.credentials.SystemCredentialsProvider.getInstance().getStore().getCredentials(com.cloudbees.plugins.credentials.domains.Domain.global()).find { it.getId().equals(CREDENTIAL_ID) }.getSecret().getPlainText()
                                                     def URL = "https://" + SECRET + "@github.com/cb-ci-templates/ci-poc-params-update.git"
-                                                    //def result = ["/bin/bash", "-c", "git ls-remote -h " + URL + " | sed 's/.*refs\\/heads\\/\\(.*\\)/\\1/'"].execute().text.tokenize();
-                                                    def result = ["/bin/bash", "-c", "git ls-remote -h " + URL + " | sed 's#.*refs/heads/##'"].execute().text.tokenize();
+                                                    def result = ["/bin/bash", "-c", "git ls-remote -h " + URL + " | awk '{print \\$2}' | sed 's|refs/heads/||'"].execute().text.tokenize();
                                                     return result                                       
                                     ''']
-                                )
-                    )
+                )
+        )
         ]
-),
-            [$class: 'JobLocalConfiguration', changeReasonComment: '']])
+)])
 pipeline {
     agent {
         kubernetes {
